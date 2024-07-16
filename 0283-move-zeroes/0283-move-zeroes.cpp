9a01becbfa121d0b467bacf277ccdae7e741d2de
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-07-16 14:30:28 +0530

    Time: 18 ms (42%), Space: 22.5 MB (7.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        for(int j=0;j<nums.size();j++)
+        int n = nums.size();
+        vector<int>v(n,0);
+        int j=0;
+        for(int i=0;i<n;i++)
         {
-            if(nums[j]!=0)
+            if(nums[i]!=0)
             {
-                swap(nums[j],nums[i]);
-                i++;
+                v[j]=nums[i];
+                j++;
             }
         }
+        nums=v;
     }
 };
