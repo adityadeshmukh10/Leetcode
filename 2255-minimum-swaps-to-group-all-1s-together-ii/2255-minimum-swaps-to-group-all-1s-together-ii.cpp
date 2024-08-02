2c31228c70c5e8a5c3521f38c1492733b599e79c
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-08-02 13:04:21 +0530

    Time: 77 ms (23.18%), Space: 90.1 MB (21.22%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,25 +1,25 @@
-class Solution {
+ class Solution {
 public:
-    int minSwaps(vector<int>& nums) {
-        int n = nums.size();
-        int ones = 0; // total no. of ones
-        for(int i=0;i<n;i++){
-            if(nums[i]==1) ones++;
-        }
-        if(ones<=1) return 0;
-        
-        int k = ones; // sliding window length
-        int maxOnes = 0; // maxOnes in sub array of length k 
-        int cnt = 0;
-        for(int i=0;i<k;i++){
-            if(nums[i]==1) cnt++;
-        }
-        maxOnes = cnt;
-        for(int i=k;i<n+k;i++){
-            if(nums[i-k]==1) cnt--; // if element removing from window is 1, then decrease count.
-            if(nums[i%n]==1) cnt++; // if element adding to window is 1, then increase count.
-            maxOnes = max(maxOnes,cnt); // maintaing maxOnes for all sub arrays of length k.
-        }
-        return(k - maxOnes); // (total length of subarray - ones in the sub array found) 
-    }
-};
+     int minSwaps(vector<int>& nums) {
+         int n = nums.size();
+         vector<int> temp(2*n);
+         for (int i = 0; i < 2 * n; i++) {
+             temp[i] = nums[i % n];
+         }
+         int totalOnes = accumulate(nums.begin(), nums.end(), 0);
+         int i = 0, j = 0;
+         int currOnes = 0, maxCount = 0;
+         while (j < 2 * n) {
+             if (temp[j] == 1) {
+                 currOnes++;
+             }
+             if (j - i + 1 > totalOnes) {
+                 currOnes -= temp[i];
+                 i++;
+             }
+             maxCount = max(maxCount, currOnes);
+             j++;
+         }
+         return totalOnes - maxCount;
+     }
+ };
