92ed6525428fb6859ca4a2bf7295c55f6ce3bfcc
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-08-08 00:00:05 +0530

    Time: 4 ms (49.19%), Space: 11.2 MB (61.34%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-    //globally declare arrays
-    string ones[20] = {"", " One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven", " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen", " Seventeen", " Eighteen", " Nineteen"};
-    string tens[10] = {"", " Ten", " Twenty", " Thirty", " Forty", " Fifty", " Sixty", " Seventy", " Eighty", " Ninety"};
-    string thousands[4] = {"", " Thousand", " Million", " Billion"};
-    //helper function
-    string helper(int n) {
-        if (n < 20) 
-            return ones[n];
-        if (n < 100) 
-            return tens[n / 10] + helper(n % 10);
-        if (n < 1000) 
-            return helper(n / 100) + " Hundred" + helper(n % 100);
-        for (int i = 3; i >= 0; i--) {
-            if (n >= pow(1000, i)) {
-                return helper(n / pow(1000, i)) + thousands[i] + helper(n % (int)pow(1000, i));
-            }
-        }
-        return "";
+    vector<string> belowTen = {"","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> belowTwenty = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    vector<string> belowHundred = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    string numberToWords(int num) {
+        if(num == 0)
+           return "Zero";
+        return find(num);
     }
 
-    string numberToWords(int num) {
-        // edge case
-        if (num == 0) 
-            return "Zero";
-        return helper(num).substr(1);
+    string find(int num)
+    {
+        string result = "";
+        if(num<10)
+            result = belowTen[num];
+        else
+            if(num<20)
+                result = belowTwenty[num-10];
+        else
+            if(num<100)
+                result = belowHundred[num/10] + " " + find(num%10);
+        else
+            if(num<1000)
+                result = find(num/100) + " Hundred " + find(num%100);
+        else
+            if(num<1000000)
+                result = find(num/1000) + " Thousand " + find(num%1000);
+        else
+            if(num<1000000000)
+                result = find(num/1000000) + " Million " + find(num%1000000);
+        else
+            result = find(num/1000000000) + " Billion " + find(num%1000000000);
+        int size = result.length();
+        if(size>0 && result[size-1]==' ')
+            result = result.substr(0, size-1);
+        return result;
     }
 };
