0035dd7d66d2bcea9b6896a547bf6e69cc62c14c
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-08-11 19:39:56 +0530

    Time: 4 ms (45.26%), Space: 12.1 MB (21.9%) - LeetHub

--- 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
+++ 0870-magic-squares-in-grid/0870-magic-squares-in-grid.cpp
@@ -1,43 +1,65 @@
 class Solution {
-    bool isValid(int i, int j, vector<vector<int>>& grid) {
-        vector<int> count(10, 0);
-        for (int x = 0; x < 3; ++x) {
-            for (int y = 0; y < 3; ++y) {
-                int num = grid[i + x][j + y];
-                if (num < 1 || num > 9 || count[num] > 0) return false;
-                count[num]++;
+public:
+    bool isMagicgrid(vector<vector<int>>& grid, int r, int c)
+    {
+        unordered_set<int> st;
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                int num = grid[r+i][c+j];
+                if(num<1 || num>9 || st.count(num))
+                {
+                    return false;
+                }
+                else
+                {
+                    st.insert(num);
+                }
             }
         }
 
-        int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
-        
-        for(int x = 0; x < 3; ++x) {
-            if(sum != (grid[i + x][j] + grid[i + x][j + 1] + grid[i + x][j + 2])) return false;
-        }
-        
-        for(int y = 0; y < 3; ++y) {
-            if(sum != (grid[i][j + y] + grid[i + 1][j + y] + grid[i + 2][j + y])) return false;
-        }
-        
-        if(sum != (grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2])) return false;
-        if(sum != (grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2])) return false;
+        int sum = grid[r][c] + grid[r][c+1] + grid[r][c+2];
+        for(int i=0;i<3;i++)
+        {
+            if(grid[r+i][c] + grid[r+i][c+1] + grid[r+i][c+2] != sum)
+            {
+                return false;
+            }
 
+            if(grid[r][c+i] + grid[r+1][c+i] + grid[r+2][c+i]!=sum)
+            {
+                return false;
+            }
+        }
+
+        if(grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] !=sum)
+        {
+            return false;
+        }
+        if(grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != sum)
+        {
+            return false;
+        }
         return true;
     }
-    
-public:
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int cnt = 0;
-        int row = grid.size();
-        int col = grid[0].size();
-        
-        if(row < 3 || col < 3) return 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
 
-        for(int i = 0; i <= row - 3; ++i) {
-            for(int j = 0; j <= col - 3; ++j) {
-                if(isValid(i, j, grid)) cnt++;
+        int count = 0;
+
+        for(int i=0;i<=rows-3;i++)
+        {
+            for(int j=0;j<=cols-3;j++)
+            {
+                if(isMagicgrid(grid,i,j))
+                {
+                    count++;
+                }
+                
             }
         }
-        return cnt;
+        return count;
     }
 };
