5ed446c04d6a01a5356ed535b12b8b39811e8b56
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-08-11 09:19:27 +0530

    Time: 14 ms (23.76%), Space: 14 MB (17.71%) - LeetHub

--- 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int i = rStart, j = cStart;
-        int diri = 0, dirj = 1; // directions to move
-        int twice = 2;
-        vector<vector<int>> res;
-        int moves = 1;
-        int next_moves = 2;
+        vector<vector<int>> directions = {{0,1},{1,0},{0,-1},{-1,0}};
+        vector<vector<int>> result;
 
-        while (res.size() < rows * cols) {
-            if (i >= 0 && i < rows && j >= 0 && j < cols) {
-                res.push_back({i, j});
+        int steps = 0;
+        int dir = 0;
+
+        result.push_back({rStart,cStart});
+
+        while(result.size()<rows*cols)
+        {
+            if(dir==0 || dir==2)
+            {
+                steps++;
             }
+            for(int count = 0;count<steps;count++)
+            {
+                rStart += directions[dir][0];
+                cStart += directions[dir][1];
 
-            i += diri;
-            j += dirj;
-            moves -= 1;
-            if (moves == 0) {
-                int temp = diri;
-                diri = dirj;
-                dirj = -temp; // right direction
-                twice -= 1;
-                if (twice == 0) {
-                    twice = 2;
-                    moves = next_moves;
-                    next_moves += 1;
-                } else {
-                    moves = next_moves - 1;
+                if(rStart >=0 && rStart<rows && cStart >=0 && cStart < cols)
+                {
+                    result.push_back({rStart,cStart});
                 }
             }
+            dir = (dir+1)%4;
         }
-        return res;
+        return result;
     }
 };
