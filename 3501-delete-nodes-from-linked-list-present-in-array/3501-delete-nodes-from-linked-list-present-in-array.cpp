96895777a36d58c0a859dbf0b6506139d432accf
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-09-15 01:24:21 +0530

    Time: 441 ms (53.35%), Space: 262.8 MB (26.77%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -11,38 +11,30 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        // Convert the array `nums` into a set for fast lookup of values to be removed.
-        set<int> st(nums.begin(), nums.end());
+        unordered_set<int> st(begin(nums),end(nums));
 
-        // Initialize a pointer `curr` to traverse the linked list.
-        ListNode* curr = head;
-        
-        // Traverse the list to find the first node whose value is NOT in the set.
-        // This node will be the new head of the modified linked list.
-        while(curr!=NULL) {
-            if(st.count(curr->val)) { 
-                // If the current node's value is in the set, skip this node.
-                curr = curr->next;
-            }
-            else break; // Stop when we find a node whose value is NOT in the set.
+        //Handle the head
+        while(head!=NULL && st.find(head->val)!=st.end())
+        {
+            ListNode *temp = head;
+            head=head->next;
+            delete(temp);
         }
 
-        // The first valid node becomes the new head of the modified list.
-        ListNode* newHead = curr;
-
-        // Traverse the rest of the linked list and remove nodes whose values exist in the set.
-        while(curr && curr->next) { 
-            if(st.count(curr->next->val)) {
-                // If the next node's value is in the set, skip the next node by adjusting pointers.
+        ListNode *curr = head;
+        while(curr!=NULL && curr->next!=NULL)
+        {
+            if(st.find(curr->next->val)!=st.end())
+            {
+                ListNode *temp = curr->next;
                 curr->next = curr->next->next;
-            } 
-            else {
-                // Move to the next node if it's not in the set.
-                curr = curr->next;
+                delete(temp);
+            }
+            else
+            {
+                curr=curr->next;
             }
         }
-
-        // Return the new head of the modified linked list.
-        return newHead;
+        return head;
     }
 };
