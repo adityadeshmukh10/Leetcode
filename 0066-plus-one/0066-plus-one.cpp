563b4f964f672c23089282f5c49b55272af53b12
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-09-16 00:14:04 +0530

    Time: 5 ms (26.96%), Space: 10.6 MB (50.17%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+      int n = digits.size() - 1;
+      
+      for (int i = n; i >= 0; --i)
+       { // traverse digits from the last element (least significant)
+       
+        if (digits[i] == 9) { 
+          digits[i] = 0;
+        } 
+        else 
+        {  // current digit is not 9 so we can safely increment by one
+          digits[i] += 1;
+          return digits;
+        }
+      }
+      
+      digits.push_back(0);
+      digits[0] = 1;
+      return digits;
+    }
+};
