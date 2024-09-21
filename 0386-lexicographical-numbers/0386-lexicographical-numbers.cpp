d561fcdf560361a553fd6bdcf72fdfca743369c6
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-09-21 11:36:21 +0530

    Time: 72 ms (22.38%), Space: 26.6 MB (20.13%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,83 +1,19 @@
 class Solution {
 public:
-    static bool comp(const string &a, const string &b) {
-        // Comparator to compare two strings in lexicographical order
-        return a + b < b + a;
-    }
-
     vector<int> lexicalOrder(int n) {
-        vector<string> v; // Vector to store numbers as strings
-        for (int i = 1; i <= n; i++) {
-            v.push_back(to_string(i)); // Convert numbers to strings
+        vector<int> v;
+        vector<string> v1;
+
+        for(int i=0;i<n;i++)
+        {
+            v1.push_back(to_string(i+1));
         }
+        sort(v1.begin(),v1.end());
 
-        // Sort strings lexicographically using the built-in string comparator
-        sort(v.begin(), v.end());
-
-        vector<int> res; // Vector to store the final result
-        for (const string &s : v) {
-            res.push_back(stoi(s)); // Convert strings back to integers
+        for(int i=0;i<v1.size();i++)
+        {
+            v.push_back(stoi(v1[i]));
         }
-
-        return res; // Return the result in lexicographical order
+        return v;
     }
 };
-
-// class Solution {
-// public:
-//     static bool comp(const string &a, const string &b) {
-//         // Comparator function to sort based on lexicographical order
-//         return a + b < b + a;
-//     }
-
-//     vector<int> lexicalOrder(int n) {
-//         vector<string> v;  // Changed vector<int> to vector<string> to handle strings for sorting
-//         for (int i = 1; i <= n; i++) {
-//             v.push_back(to_string(i));  // Convert integers to strings
-//         }
-
-//         // Sort the numbers lexicographically using the custom comparator
-//         sort(v.begin(), v.end());
-
-//         vector<int> res;
-//         for (const string &s : v) {
-//             res.push_back(stoi(s));  // Convert sorted strings back to integers
-//         }
-
-//         return res;      
-//     }
-// };
-
-
-
-
-
-
-
-// class Solution {
-// public:
-//     static bool comp(string a,string b)
-//     {
-//         return a+b >b+a;
-//     }
-//     string largestNumber(vector<int>& nums) 
-//     {
-//         vector<string>v;
-//         for(int i=0;i<nums.size();i++)
-//         {
-//             v.push_back(to_string(nums[i]));
-//         }
-//         sort(v.begin(),v.end(),comp);
-//         if(v[0]=="0")
-//         {
-//             return "0";
-//         }
-           
-//         string ans="";
-//         for(string x: v)
-//         {
-//             ans+=x;
-//         }
-//         return ans;
-//     }
-// };
