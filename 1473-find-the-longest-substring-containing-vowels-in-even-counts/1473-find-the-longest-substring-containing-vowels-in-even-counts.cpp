5674b034fa4179acb414d2c5206d28002362dea8
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-09-27 18:50:38 +0530

    Time: 782 ms (5.02%), Space: 21.2 MB (9.47%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,15 +1,54 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        unordered_map<int, int> m;
-        m.insert({0, -1});
-        int ans = 0, cur = 0;
-        for (int i = 0; i < s.length(); i++) {
-            auto n = string("aeiou").find(s[i]);
-            if (n != string::npos) cur ^= 1 << n;
-            if (!m.count(cur)) m[cur] = i;
-            ans = max(ans, i - m[cur]);
+        unordered_map<string,int> mp;
+
+        vector<int> state(5,0);
+        
+        string currstate = "00000";
+        mp[currstate] = -1;
+
+        int result = 0;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] == 'a')
+            {
+                state[0] = (state[0]+1) % 2;
+            }
+            else if(s[i] == 'e')
+            {
+                state[1] = (state[1]+1) % 2;
+            }
+            else if(s[i] == 'i')
+            {
+                state[2] = (state[2]+1) % 2;
+            }
+            else if(s[i] == 'o')
+            {
+                state[3] = (state[3]+1) % 2;
+            }
+            else if(s[i] == 'u')
+            {
+                state[4] = (state[4]+1) % 2;
+            }
+
+            currstate = "";
+            for(int j=0;j<5;j++)
+            {
+                currstate += to_string(state[j]);
+            }
+
+            if(mp.find(currstate)!=mp.end())
+            {
+                result = max(result, i - mp[currstate]);
+            }
+            else
+            {
+                mp[currstate] = i;
+            }
         }
-        return ans;
+
+        return result;
     }
 };
