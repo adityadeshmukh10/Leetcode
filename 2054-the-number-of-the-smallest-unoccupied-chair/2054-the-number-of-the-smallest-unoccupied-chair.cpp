2095a33b9fd22cf129c048292ab4e42eb21265b5
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-10-11 19:38:45 +0530

    Time: 304 ms (13.75%), Space: 54.4 MB (98.28%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int n = times.size();
+
+        vector<int> chairs(n, -1);
+
+        int targetFriendArrivalTime = times[targetFriend][0];
+
+        sort(begin(times), end(times));
+
+        for(vector<int>&time: times)
+        {
+            int arrival = time[0];
+            int depart = time[1];
+
+            for(int i=0;i<n;i++)
+            {
+                if(chairs[i]<=arrival)
+                {
+                    chairs[i] = depart;
+
+                    if(arrival == targetFriendArrivalTime)
+                    {
+                        return i;
+                    }
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+};
