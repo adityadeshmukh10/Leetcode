55fab6be9ff0d563f9edef3a88e9cb168d1c1b3b
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-10-12 12:17:44 +0530

    Time: 123 ms (96.25%), Space: 55 MB (93.86%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
+    typedef pair<int, int> P;
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n = times.size();
 
-        vector<int> chairs(n, -1);
+        priority_queue<P, vector<P>, greater<P>> occupied;  // To track occupied chairs (departure time, chair number)
+        priority_queue<int, vector<int>, greater<int>> free;  // To track available free chairs
 
         int targetFriendArrivalTime = times[targetFriend][0];
 
-        sort(begin(times), end(times));
+        int chairno = 0;  // Keeps track of chair assignment
+        sort(begin(times), end(times));  // Sort times based on arrival time
 
-        for(vector<int>&time: times)
-        {
-            int arrival = time[0];
-            int depart = time[1];
+        for (int i = 0; i < n; i++) {
+            int arrival = times[i][0];
+            int depart = times[i][1];
 
-            for(int i=0;i<n;i++)
-            {
-                if(chairs[i]<=arrival)
-                {
-                    chairs[i] = depart;
+            // Release all chairs that are free before current friend's arrival time
+            while (!occupied.empty() && occupied.top().first <= arrival) {
+                free.push(occupied.top().second);
+                occupied.pop();
+            }
 
-                    if(arrival == targetFriendArrivalTime)
-                    {
-                        return i;
-                    }
-                    break;
+            // If no free chairs, assign a new chair
+            if (free.empty()) {
+                occupied.push({depart, chairno});
+                if (arrival == targetFriendArrivalTime) {
+                    return chairno;  // Return if it's the target friend's arrival
                 }
+                chairno++;  // Increment chair number for the next friend
+            }
+            // Assign the least available chair
+            else {
+                int leastChairAvailable = free.top();
+                free.pop();
+                if (arrival == targetFriendArrivalTime) {
+                    return leastChairAvailable;  // Return if it's the target friend's arrival
+                }
+                occupied.push({depart, leastChairAvailable});
             }
         }
         return -1;
