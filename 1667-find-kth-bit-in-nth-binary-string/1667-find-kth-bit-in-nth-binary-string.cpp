395e2553b0bda81fa73a06e7aee2b1a669cd7570
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2024-10-19 08:18:31 +0530

    Time: 0 ms (100%), Space: 7.3 MB (87.54%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,31 +1,72 @@
 class Solution {
 public:    
     
-    string invert(string x){
-        for(int i=0; i<x.size(); i++){
-            if(x[i]=='0') x[i] ='1';
-            else x[i] ='0';
-        }            
-        return x;
-    }      
-    
-    string formSequence(int n){
-        if(n==0)
-            return "0";                
-        
-        string s = formSequence(n-1);
-        string result="";        
-        
-        // Compute formSequence(n)                
-        string rev = invert(s);
-        reverse(rev.begin(), rev.end());
-        result = s + "1" + rev;
-        
-        return result;
+   int form(int n, int k){
+        if(n == 1 && k == 1) 
+            return 0;
+       
+        int mid = (1 << (n-1));
+       
+        if(k < mid) 
+            return form(n - 1, k);
+        else if(k == mid) 
+            return 1;
+        else 
+            return !( form(n - 1, (mid << 1) - k));
     }
     
-    char findKthBit(int n, int k) {
-        string s = formSequence(n);
-        return s[k-1];
+    char findKthBit(int n, int k) {                       
+        return (form(n,k) + '0');
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     string invert(string x){
+//         for(int i=0; i<x.size(); i++){
+//             if(x[i]=='0') x[i] ='1';
+//             else x[i] ='0';
+//         }            
+//         return x;
+//     } 
+//     string formsequence(int n)
+//     {
+//         if(n==0)
+//         {
+//             return "0";
+//         }
+//         string s = formsequence(n-1);
+//         string result = "";
+
+//         //compute formsequence
+//         string rev = invert(s);
+//         reverse(rev.begin(),rev.end());
+//         result = s + "1" + rev;
+
+//         return result;
+//     }
+//     char findKthBit(int n, int k) {
+//         return s = formsequence(n)
+//         return s[k-1];
+//     }
+// };
