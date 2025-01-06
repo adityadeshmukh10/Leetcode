424e37f737c258f0bd1e7f6b3a7cef63ca182b15
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2025-01-06 17:40:12 +0530

    Time: 0 ms (100%), Space: 23.5 MB (5.66%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int j=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1]){
-                nums[j]=nums[i];
-                j++;
-            }
+        if (nums.empty()) return 0; // Handle edge case for empty input
+
+        // Use a set to store unique elements
+        set<int> st;
+        for (int i = 0; i < nums.size(); i++) {
+            st.insert(nums[i]);
         }
-        return j;
+
+        // Overwrite nums with unique elements and fill the rest with '_'
+        int k = 0; // Counter for unique elements
+        for (int val : st) {
+            nums[k++] = val; // Place unique elements at the start of nums
+        }
+
+        while (k < nums.size()) {
+            nums[k++] = '_'; // Replace remaining positions with '_'
+        }
+
+        return st.size(); // Return the count of unique elements
     }
 };
