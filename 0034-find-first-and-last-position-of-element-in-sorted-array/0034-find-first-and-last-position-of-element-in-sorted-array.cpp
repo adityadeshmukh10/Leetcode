0137807879757c06ab3530ff59250386601290a5
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2025-02-05 19:19:45 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,62 +1,26 @@
-int firstoccurence(vector<int>& nums,int target){
-    int low = 0;
-    int index1 = -1 ;
-    int high = nums.size()-1;
-   while(high>=low){
-        int mid = low +(high-low)/2;
-        if(nums[mid]==target){
-            high = mid-1;
-            index1 = mid ;
-             
-             
-        }
-        else if (nums[mid]>target){
-            high = mid -1 ; 
-        }
-        else{
-            low = mid + 1;
-        }
-    
-    
-    }
-    return index1 ; 
-}
-
-int lastoccurence(vector<int>& nums,int target){
-    int low = 0;
-    int index2 = -1;
-    int high = nums.size()-1;
-   while(high>=low){
-        int mid = low +(high-low)/2;
-        if(nums[mid]==target){
-            low = mid+1;
-            index2 = mid ; 
-             
-        }
-        else if (nums[mid]>target){
-            high = mid -1 ; 
-        }
-        else{
-            low = mid + 1;
-        }
-
-    }
-    return index2 ; 
-
-}
-
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> v = {-1,-1};
+        int n = nums.size();
 
-        vector<int> res;
-
-        int x = firstoccurence(nums,  target);
-        int y = lastoccurence( nums,  target);
-
-        res.push_back(x);
-        res.push_back(y);
-        
-        return res;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==target)
+            {
+                v[0]=i;
+                break;
+            }
+            
+        }
+        for(int j=n-1;j>=0;j--)
+        {
+            if(nums[j]==target)
+            {
+                v[1]=j;
+                break;
+            }
+        }
+        return v;
     }
 };
