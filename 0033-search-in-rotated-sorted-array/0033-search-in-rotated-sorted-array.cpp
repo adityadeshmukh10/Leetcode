28fe9ef86044cbbc15abee5e5cc173ba80fbd588
TheBeliever <110899566+adityadeshmukh10@users.noreply.github.com>
2025-02-10 22:05:21 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.13%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    int getPivot(vector<int>&nums){
-        int s=0,e=nums.size()-1;
-        int mid = s+(e-s)/2;
-        while(s<e){
-            if(nums[mid]>=nums[0]){
-                s=mid+1;
-            }
-            else{
-                e=mid;
-            }
-            mid = s+(e-s)/2;
-        }
-        return s;
-    }
-    int binarySearch(int s,int e, vector<int>&nums, int target){
-        int mid = s+(e-s)/2;
-        while(s<=e){
-            mid = s+(e-s)/2;
-            if(nums[mid]==target){
+    int search(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        int st=0;
+        int end=n-1;
+
+        while(st<=end)
+        {
+            int mid = st+(end-st)/2;
+            if(nums[mid]==target)
+            {
                 return mid;
             }
-            else if(nums[mid]>target){
-                e = mid-1;
+            if(nums[st]<=nums[mid])
+            {
+                if(nums[st]<=target && target<=nums[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    st = mid + 1;
+                }
             }
-            else{
-                s= mid+1;
+            else
+            {
+                if(nums[mid]<target && target <=nums[end])
+                {
+                    st = mid+1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
             }
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int pivot = getPivot(nums);
-        if(nums[pivot]<=target && nums[n-1]>=target){
-            return binarySearch(pivot,n-1,nums,target);
-        }
-        return binarySearch(0,pivot-1,nums,target);
     }
 };
